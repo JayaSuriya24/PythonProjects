a5621d40250503ad74c0246b72e0337aac94d82c
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 16:35:06 -0500

    Comparison Operators

---
+++ PythonProgrammingBasic/Operators/ComparisonOperator.py
@@ -0,0 +1,26 @@
+
+number1 = int(input('Enter the First number :'))
+
+number2 = int(input('Enter the Second number :'))
+
+a = number1
+
+b = number2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
