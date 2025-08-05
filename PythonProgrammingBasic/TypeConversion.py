ee182e86a2f36a9924a222d737819cf30beb22de
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 11:39:16 -0500

    Variables Type conversion

---
+++ PythonProgrammingBasic/TypeConversion.py
@@ -0,0 +1,21 @@
+#Type conversion
+#FROM ONE DATA TYPE TO ANOTHER DATA TYPE
+
+print('TYPE CONVERSION PROGRAM')
+
+number1 = '10'
+
+number2 = '20'
+
+print("The value of number 1 and 2 is :",number1,"and",number2)
+print("The data type of number 1 and 2 is :",type(number1),"and",type(number2))
+
+num1 = int(number1)
+num2 = int(number2)
+
+print('---------------------------------------------')
+print("The string value is converted as integer value")
+print('---------------------------------------------')
+
+print("The value of num 1 and 2 is :",num1,"and",num2)
+print("The data type of num 1 and 2 is :",type(num1),"and",type(num2))
