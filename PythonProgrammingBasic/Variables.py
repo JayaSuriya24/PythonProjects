f534152bf533a03564668255b7c882cf74c25b71
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 11:05:05 -0500

    Variable of python

---
+++ PythonProgrammingBasic/Variables.py
@@ -0,0 +1,43 @@
+# declare a variable
+name = "suriya"
+
+# Print the name
+print(name)
+print(type(name))
+
+age = 24
+
+print(age)
+print(type(age))
+
+height = 5.11
+
+print(height)
+print(type(height))
+
+print('--------------------------------------------------------------------------------')
+print("ASSIGNING MULTIPLE VARIABLES TO THE MULTIPLE VALUES ---------- In same data type")
+print('--------------------------------------------------------------------------------')
+
+a,b,c ='hari','guhan','sudhaman'
+
+print(a,"",b,"",c)
+print(type(a),type(b),type(c))
+
+a1,b1,c1 = 30,26,26
+
+print(a1,"",b1,"",c1)
+print(type(a1),type(b1),type(c1))
+
+a2,b2,c2 = 5.10,4.12,5.12
+
+print(a2,"",b2,"",c2)
+print(type(a2),type(b2),type(c2))
+print('-------------------------------------------------------------------------------------')
+print("ASSIGNING MULTIPLE VARIABLES TO THE MULTIPLE VALUES ---------- In different data type")
+print('-------------------------------------------------------------------------------------')
+
+x,y,z = 'suriya', 24 , 5.12
+
+print(x,"",y,"",z)
+print(type(x),type(y),type(z))
