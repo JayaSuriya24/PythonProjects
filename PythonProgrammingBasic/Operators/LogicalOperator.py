24885d6d87f1a8e8f1b4a524900b3c2f87a3133e
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 16:45:00 -0500

    Logical Operators

---
+++ PythonProgrammingBasic/Operators/LogicalOperator.py
@@ -0,0 +1,15 @@
+number1 = int(input('Enter the First number :'))
+
+number2 = int(input('Enter the Second number :'))
+
+a = number1
+b = number2
+
+# AND (Return true if both the conditions are true else return false)
+print((a > 10) and (b >= 10))
+
+# OR (Return true at least any one of the condition is true)
+print((a == 10) or (a >= 10))
+
+# NOT (Return true if the condition is false)
+print(not (a==10))
