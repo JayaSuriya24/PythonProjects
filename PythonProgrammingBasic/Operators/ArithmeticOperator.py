c235d14baddee94e39d981958443249ef39666c4
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 15:52:37 -0500

    Arithmetic Operators

---
+++ PythonProgrammingBasic/Operators/ArithmeticOperator.py
@@ -0,0 +1,50 @@
+
+#Arithmetic operators
+print('ENTER TWO NUMBER TO PERFORM THE ARITHMETIC OPERATIONS')
+print('-----------------------------------------------------')
+number1 =  int(input('Enter the First number :'))
+number2 = int(input('Enter the Second number :'))
+
+print(number1,' is the first number','//',number2,' is the second number')
+
+print('-------')
+print('ADD')
+print('-------')
+add = number1 + number2
+print('Addition =',add )
+
+print('-------')
+print('SUB')
+print('-------')
+sub = number1 - number2
+print('Subtraction =',sub )
+
+print('-------')
+print('MUL')
+print('-------')
+mul = number1 * number2
+print('Multiplication =',mul )
+
+print('-------')
+print('DIV')
+print('-------')
+div = number1 / number2
+print('division =',div )
+
+print('-------')
+print('FLOOR DIV')
+print('-------')
+floor_div = number1 // number2
+print('Floor Dvision =',floor_div )
+
+print('-------')
+print('MODULO')
+print('-------')
+modulo = number1 % number2
+print('Modulo =',modulo )
+
+print('-------')
+print('POWER')
+print('-------')
+pow = number1 ** number2
+print('Power =',pow )
