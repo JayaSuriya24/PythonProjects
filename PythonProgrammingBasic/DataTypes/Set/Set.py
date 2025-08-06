f5dd31eb72305d071009e01ed2e2fd06fc4764fd
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-06 17:21:52 -0500

    Set programs

---
+++ PythonProgrammingBasic/DataTypes/Set/Set.py
@@ -0,0 +1,21 @@
+# Set are created with the curly braces ({})
+# Set can not be mutable
+# The values inside the set can not be duplicated
+
+
+# Set of integers
+numbers = {1,2,3,4,5,6,7,2,4,6}
+
+print('Set of number :',numbers)
+
+#Set of string values
+letters = {'a','b','c','d','e','f','g'}
+
+print('Set of Strings :',letters)
+
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
+
+#  empty_set - an empty set created using set()
+#  empty_dictionary - an empty dictionary created using {}
