5848530ae511975d63214001dbe48ab15574bd3e
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-06 11:36:09 -0500

    Nested_if flow control

---
+++ PythonProgrammingBasic/FlowControl/Nested_if.py
@@ -0,0 +1,11 @@
+number = int(input('Enter the number :'))
+
+# This if statement will allow to the inner-if when thw condition is true
+if number >= 0:
+    #The inner-if will check the condition and execute respectively
+    if number == 0:
+        print('The number o')
+    else: # else statement of inner-if
+        print('the number is positive')
+else: #else statement of outer if
+    print('number is negative')
