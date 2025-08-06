71ddc0448f1379fde893b0452fe77b70a5a8f835
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-06 11:53:50 -0500

    For loop program is created

---
+++ PythonProgrammingBasic/FlowControl/For_Loop/For_Loop.py
@@ -0,0 +1,7 @@
+# The list of strings are given in the nameList variable
+nameList = ['hari','guhan','sudhaman']
+
+# name is initialized for nameList
+for name in nameList:
+    #Calling the names in the list one-by-one through the loop statement
+    print(name)
