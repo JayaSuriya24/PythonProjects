7cb040e8ee7b439137f906e363888726e7c32659
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-05 19:26:08 -0500

    if_else flow control

---
+++ FlowControl/If_else.py
@@ -0,0 +1,13 @@
+# If the condition is true inside the (if condition) execute the block of code  written inside the if-block
+# If the condition is False program runs out of the if-block
+
+# when the variable written inside the parenthesis give the value of the variable
+
+number = int(input('Enter a number: '))
+
+# check if number is greater than 0
+
+if number > 0:
+    print(f'{number} is a positive number.')
+
+print('A statement outside the if statement.')
