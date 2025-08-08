acc1c30e07cf16bb673e7859460788785a3f08eb
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-08 10:17:04 -0500

    dictionary programs are completed

---
+++ PythonProgrammingBasic/DataTypes/Dictionary/Dictionary.py
@@ -0,0 +1,10 @@
+# Each item in a dictionary is a key-value pair (consisting of a key and a value).
+# creating a dictionary
+country_capitals = {
+  "Germany": "Berlin",
+  "Canada": "Ottawa",
+  "England": "London"
+}
+
+# printing the dictionary
+print(country_capitals)
