738d98e41109a0730d3aefff92c1b581fc42c7c7
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-12 17:49:28 -0500

    Python function arguments from the user input

---
+++ PythonProgrammingBasic/Functions/user_input.py
@@ -0,0 +1,11 @@
+# Getting the values from the user for the function
+
+num1 = int(input('Enter the first number :-'))
+num2 = int(input('Enter the second number:-'))
+
+def add(a,b):
+    sum = a + b
+    print('Sum of two integer:',sum)
+
+#Call the function
+add(num1,num2)
