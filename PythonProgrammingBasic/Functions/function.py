cd11ec3a2866bce103fa47667110ef7830847046
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-12 17:36:14 -0500

    Python function

---
+++ PythonProgrammingBasic/Functions/function.py
@@ -0,0 +1,9 @@
+# Creating a Function
+# def is the keyword is used to create the function()
+
+def function():
+    print('Hello world!')
+
+print('Print the output through the function call:-')
+#  Call Function
+function()
