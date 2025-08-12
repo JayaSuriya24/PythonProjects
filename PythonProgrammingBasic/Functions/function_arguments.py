fba4270db4537a3ad7b30038ffe998658cfddb58
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-12 17:41:46 -0500

    Python function arguments

---
+++ PythonProgrammingBasic/Functions/function_arguments.py
@@ -0,0 +1,9 @@
+# Passing the arguments inside the functions
+
+def addfunction(a,b):
+    add = a + b
+    print(a,'+',b,'=',add)
+
+#Function call
+print('the output of argument function:-')
+addfunction(1,2)
