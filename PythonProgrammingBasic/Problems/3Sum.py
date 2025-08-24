fe5e41f2791972e17ec2287bb425704a710ef51f
jayasuriyamanickavasagam <jayasuriya2410@gmail.com>
2025-08-24 18:25:20 -0500

    LeedCode three sum

---
+++ PythonProgrammingBasic/Problems/3Sum.py
@@ -0,0 +1,27 @@
+nums = [-1, 0, 1, 2, -1, -4]
+
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+
+        results = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+                left, right = i + 1, len(nums) - 1
+                while left < right:
+                    current_sum = nums[i] + nums[left] + nums[right]
+                    if current_sum == 0:
+                        results.append([nums[i], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                            while left < right and nums[right] == nums[right + 1]:
+                                right -= 1
+                    elif current_sum < 0:
+                        left += 1
+                    else:
+                        right -= 1
+        return results
